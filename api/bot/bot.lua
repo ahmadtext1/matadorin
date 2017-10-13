f37bb6de193fb1cb06883f39f4cbe71f60f4f1e7
ahmadtext1 <32554534+ahmadtext1@users.noreply.github.com>
2017-10-13 08:23:08 +0330

    Update bot.lua

--- api/bot/bot.lua
+++ api/bot/bot.lua
@@ -1,8 +1,8 @@
 package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
-bot_token = "Tokan" --Put You Token Here
+bot_token = "464766979:AAH1RtVQvfmJ-lJ1WGofqGMe4BDyyY108NA" --Put You Token Here
 send_api = "https://api.telegram.org/bot"..bot_token
-sudo_id = 000000
+sudo_id = 311080101
 require('./bot/methods')
 require('./bot/utils')
 require('./libs/JSON')
@@ -162,7 +162,7 @@ function create_config( )
     "MRCore",
     "Plugins"
     },
-    sudo_users = {377450049,000000,000000,000000,000000,000000},--Sudo users
+    sudo_users = {311080101,224390699},--Sudo users
     admins = {},
     disabled_channels = {},
     moderation = {data = ''..MaTaDoRTeaM..'/data/moderation.json'},
